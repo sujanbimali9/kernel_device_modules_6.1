f3e1b3a3345f3a53f3034bc832c64b2fb3aeaf06
Hongjiang He <Hongjiang.He@mediatek.com>
2024-06-18 10:31:41 +0800

    [ALPS09032958] DISP: fix cwb disconnect CMDQ err issue
    [Description]
    1.it lead to CMDQ err if cwb disconnect when enter idle(CMD mode),
    because cwb disconnect will use CMDQ
    (cherry pick from 8908288)
    2.fix lcm reset bug
    
    [Test]
    bootup success
    no CMDQ err when WFD scenario
    
    MTK-Commit-Id: 097f37f1b983b33e88b2f0cde87dd63bf1472f86
    
    Signed-off-by: Hongjiang He <Hongjiang.He@mediatek.com>
    CR-Id: ALPS09032958
    Feature: display_mobile
    Change-Id: Ide3a6d69db15dacb616fdcc5d3e1bc238fae3d6f

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_debug.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_debug.c
@@ -3269,6 +3269,7 @@ static void process_dbg_opt(const char *opt)
 			DDPINFO("cannot find output component\n");
 			return;
 		}
+		DDP_MUTEX_LOCK(&mtk_crtc->lock, __func__, __LINE__);
 		enable = 1;
 		comp->funcs->io_cmd(comp, NULL, LCM_RESET, &enable);
 		msleep(20);
@@ -3277,6 +3278,7 @@ static void process_dbg_opt(const char *opt)
 		msleep(20);
 		enable = 1;
 		comp->funcs->io_cmd(comp, NULL, LCM_RESET, &enable);
+		DDP_MUTEX_UNLOCK(&mtk_crtc->lock, __func__, __LINE__);
 	} else if (strncmp(opt, "lcm1_reset", 10) == 0) {
 		struct mtk_ddp_comp *comp;
 		struct drm_crtc *crtc;
@@ -3307,6 +3309,7 @@ static void process_dbg_opt(const char *opt)
 			DDPINFO("cannot find output component\n");
 			return;
 		}
+		DDP_MUTEX_LOCK(&mtk_crtc->lock, __func__, __LINE__);
 		enable = 1;
 		comp->funcs->io_cmd(comp, NULL, LCM_RESET, &enable);
 		msleep(20);
@@ -3315,6 +3318,7 @@ static void process_dbg_opt(const char *opt)
 		msleep(20);
 		enable = 1;
 		comp->funcs->io_cmd(comp, NULL, LCM_RESET, &enable);
+		DDP_MUTEX_UNLOCK(&mtk_crtc->lock, __func__, __LINE__);
 	} else if (strncmp(opt, "backlight:", 10) == 0) {
 		unsigned int level;
 		int ret;
