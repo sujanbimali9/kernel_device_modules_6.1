a0e22428df4c01f244f069ef373fb9fd950c5be9
pina.chen <pina.chen@mediatek.com>
2024-07-16 19:16:38 +0800

    [ALPS09087833] vcodec: add error debug log
    [Description]
    Add more error debug log for ipi handler vcu not exist
    
    [Test]
    test pass
    
    MTK-Commit-Id: a22a5961e4911dedf8c899383c37baebd5d9f0d0
    
    Signed-off-by: pina.chen <pina.chen@mediatek.com>
    CR-Id: ALPS09087833
    Feature: Video Driver
    Change-Id: I75427c93053e987910413fe272a6ad5835ec9d36
    (cherry picked from commit f90a6373488cdc61967dbd6c3f8203393e85ca99)

--- drivers/media/platform/mtk-vcodec/vdec/vdec_vcp_if.c
+++ drivers/media/platform/mtk-vcodec/vdec/vdec_vcp_if.c
@@ -678,12 +678,22 @@ int vcp_dec_ipi_handler(void *arg)
 				break;
 			}
 		}
-		mutex_unlock(&dev->ctx_mutex);
 		if (!msg_valid) {
-			mtk_v4l2_err(" msg msg_id %X vcu not exist %p\n", msg->msg_id, vcu);
+			if (vcu) {
+				inst = container_of(vcu, struct vdec_inst, vcu);
+				ctx = vcu->ctx;
+			} else {
+				inst = NULL;
+				ctx = NULL;
+			}
+			mtk_v4l2_err(" msg msg_id %X vcu not exist 0x%lx (ctx 0x%lx, inst 0x%lx)\n", msg->msg_id,
+				(unsigned long)vcu, (unsigned long)ctx, (unsigned long)inst);
+			mtk_vcodec_dump_ctx_list(dev, 0);
+			mutex_unlock(&dev->ctx_mutex);
 			vdec_vcp_free_mq_node(dev, mq_node);
 			continue;
 		}
+		mutex_unlock(&dev->ctx_mutex);
 
 		if (vcu->abort || vcu->daemon_pid != get_vcp_generation()) {
 			mtk_vcodec_err(vcu, " msg msg_id %X vcu abort %d %d\n",
