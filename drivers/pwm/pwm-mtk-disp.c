9946538404e8f1e29ce2cfb56c2cc3dd1d931d20
Huijuan Xie <huijuan.xie@mediatek.com>
2024-08-13 14:28:57 +0800

    [ALPS09185706] disp-pwm: Add support for mt6897-disp-pwm[2/3]
    [Description]
    The mt6897 device was not included in the pwm-mtk-disp drivers
    compatibility list. Add mt6897 device to support adjust backlight
    by disp-pwm.
    
    [Test]
    Build pass
    
    MTK-Commit-Id: e240cbf080ab949e30e4493d03d6aaaaf7155178
    
    Signed-off-by: Huijuan Xie <huijuan.xie@mediatek.com>
    CR-Id: ALPS09185706
    Feature: Always On Display
    Change-Id: If60553513e1cec3e6f2b0cdc9e82fc5196f639d7

--- drivers/pwm/pwm-mtk-disp.c
+++ drivers/pwm/pwm-mtk-disp.c
@@ -332,6 +332,7 @@ static const struct of_device_id mtk_disp_pwm_of_match[] = {
 	{ .compatible = "mediatek,mt8183-disp-pwm", .data = &mt8183_pwm_data},
 	{ .compatible = "mediatek,mt6886-disp-pwm", .data = &mt6799_pwm_data},
 	{ .compatible = "mediatek,mt6835-disp-pwm", .data = &mt6799_pwm_data},
+	{ .compatible = "mediatek,mt6897-disp-pwm", .data = &mt8183_pwm_data},
 	{ }
 };
 MODULE_DEVICE_TABLE(of, mtk_disp_pwm_of_match);
