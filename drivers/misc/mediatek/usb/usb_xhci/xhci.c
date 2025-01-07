a4931524c2e4ab48ed3704ebe288145540f97d5e
Denis Hsu <denis.hsu@mediatek.com>
2025-01-07 10:49:30 +0800

    [ALPS09653565] xhci: Implement xhci_handshake_check_state() helper
    [Description]
    In some situations where xhci removal happens parallel to xhci_handshake,
    we encounter a scenario where the xhci_handshake cant succeed, and it
    polls until timeout.
    
    If xhci_handshake runs until timeout it can on some platforms result in
    a long wait which might lead to a watchdog timeout.
    
    Add a helper that checks xhci status during the handshake, and exits if
    set state is entered. Use this helper in places where xhci_handshake is
    called unlocked and has a long timeout.
    
    [Test]
    xhci driver probe/ remove well
    
    MTK-Commit-Id: f73f963ee594ea3be84daa8235125f615bdb77bb
    
    CR-Id: ALPS09653565
    Feature: usb_ssusb
    Change-Id: Idbf8caca952af4636854335c49e6cb86e1a0a56c
    Signed-off-by: Denis Hsu <denis.hsu@mediatek.com>

--- drivers/misc/mediatek/usb/usb_xhci/xhci.c
+++ drivers/misc/mediatek/usb/usb_xhci/xhci.c
@@ -90,6 +90,29 @@ int xhci_handshake(void __iomem *ptr, u32 mask, u32 done, u64 timeout_us)
 }
 
 /*
+ * xhci_handshake_check_state - same as xhci_handshake but takes an additional
+ * exit_state parameter, and bails out with an error immediately when xhc_state
+ * has exit_state flag set.
+ */
+
+int xhci_handshake_check_state(struct xhci_hcd *xhci, void __iomem *ptr,
+						u32 mask, u32 done, u64 timeout_us, unsigned int exit_state)
+{
+	u32	result;
+	int	ret;
+
+	ret = readl_poll_timeout_atomic(ptr, result,
+					(result & mask) == done ||
+					result == U32_MAX ||
+					xhci->xhc_state & exit_state,
+					1, timeout_us);
+	if (result == U32_MAX || xhci->xhc_state & exit_state)		/* card removed */
+		return -ENODEV;
+
+	return ret;
+}
+
+/*
  * Disable interrupts and begin the xHCI halting process.
  */
 void xhci_quiesce(struct xhci_hcd *xhci)
@@ -208,7 +231,8 @@ int xhci_reset(struct xhci_hcd *xhci, u64 timeout_us)
 	if (xhci->quirks & XHCI_INTEL_HOST)
 		udelay(1000);
 
-	ret = xhci_handshake(&xhci->op_regs->command, CMD_RESET, 0, timeout_us);
+	ret = xhci_handshake_check_state(xhci, &xhci->op_regs->command, CMD_RESET, 0,
+		timeout_us, XHCI_STATE_REMOVING);
 	if (ret)
 		return ret;
 
