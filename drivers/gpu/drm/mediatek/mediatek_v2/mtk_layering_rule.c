fee8f778e963d876fa9a7079c696cd2227aa081b
ruofan Shang <ruofan.Shang@mediatek.com>
2025-07-08 22:31:26 +0800

    [ALPS10117340] display: mt8775: change hrt layer table
    [Description]
    change hrt layer table
    
    [Test]
    build pass and test ok
    
    MTK-Commit-Id: f2af073fdaca7995cd417baea1b1a4a4c9703470
    
    Signed-off-by: ruofan Shang <ruofan.Shang@mediatek.com>
    CR-Id: ALPS10117340
    Feature: display_mobile
    Change-Id: I8ee43b8c1e50212970a339dc3ec3de248a3b1097

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_layering_rule.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_layering_rule.c
@@ -59,7 +59,11 @@ static uint16_t layer_mapping_table[HRT_TB_NUM] = {
 #endif
 };
 static uint16_t layer_mapping_table_mt6985[HRT_TB_NUM] = {
+#if IS_ENABLED(CONFIG_MTK_DISPLAY_DUAL_PIPE_DUAL_PORT_SUPPORT)
+	0x0003, 0x0078, 0x0078, 0x0001
+#else
 	0x0003, 0x007E, 0x007A, 0x0001
+#endif
 }; //0x0006:one OVL_2L, 0x007E:three OVL_2L
 static uint16_t layer_mapping_table_vds_switch[HRT_TB_NUM] = {
 	0x0078, 0x0078, 0x0078, 0x0078
@@ -87,7 +91,11 @@ static uint16_t ovl_mapping_table[HRT_TB_NUM] = {
 #endif
 };
 static uint16_t ovl_mapping_table_mt6985[HRT_TB_NUM] = {
+#if IS_ENABLED(CONFIG_MTK_DISPLAY_DUAL_PIPE_DUAL_PORT_SUPPORT)
+	0x0002, 0x0057, 0x0057, 0x0001
+#else
 	0x0002, 0x0055, 0x0055, 0x0001
+#endif
 }; //0x0005:one OVL_2L, 0x0055:three OVL_2L
 static uint16_t ovl_mapping_tb_vds_switch[HRT_TB_NUM] = {
 	0x0045, 0x0045, 0x0045, 0x0045
