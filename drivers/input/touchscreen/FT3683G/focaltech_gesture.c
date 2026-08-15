f2dec9a4c676cb8a457bccf5c10ae67b8022a1af
Alexander Koskovich <akoskovich@pm.me>
2026-08-15 18:37:48 +0000

    focaltech: Add single tap to wake sysfs nodes

--- drivers/input/touchscreen/FT3683G/focaltech_gesture.c
+++ drivers/input/touchscreen/FT3683G/focaltech_gesture.c
@@ -51,7 +51,6 @@
 #define KEY_GESTURE_V                           KEY_V
 #define KEY_GESTURE_C                           KEY_C
 #define KEY_GESTURE_Z                           KEY_Z
-#define KEY_GESTURE_WAKEUP                     KEY_WAKEUP
 #define KEY_PALM_TO_SLEEP                       252
 
 #define GESTURE_LEFT                            0x20
@@ -223,6 +222,57 @@ static ssize_t fts_gesture_bm_store(
     return count;
 }
 
+static ssize_t fts_gesture_single_tap_enabled_show(struct device *dev,
+                                    struct device_attribute *attr, char *buf)
+{
+    struct fts_ts_data *ts_data = dev_get_drvdata(dev);
+
+    if (!ts_data)
+        return -ENODEV;
+
+    return scnprintf(buf, PAGE_SIZE, "%d\n", ts_data->single_tap_enabled ? 1 : 0);
+}
+
+static ssize_t fts_gesture_single_tap_enabled_store(struct device *dev,
+                                     struct device_attribute *attr,
+                                     const char *buf, size_t count)
+{
+    struct fts_ts_data *ts_data = dev_get_drvdata(dev);
+    unsigned long val;
+    int error;
+
+    if (!ts_data)
+        return -ENODEV;
+
+    error = kstrtoul(buf, 0, &val);
+    if (error)
+        return error;
+
+    if (val != 0 && val != 1)
+        return -EINVAL;
+
+    mutex_lock(&ts_data->input_dev->mutex);
+    ts_data->gesture_support = val;
+    ts_data->single_tap_enabled = val;
+
+    (val ? fts_gesture_recovery(ts_data)
+         : fts_write_reg(FTS_REG_GESTURE_EN, DISABLE));
+    mutex_unlock(&ts_data->input_dev->mutex);
+
+    return count;
+}
+
+static ssize_t fts_gesture_single_tap_pressed_show(struct device *dev,
+                                    struct device_attribute *attr, char *buf)
+{
+    struct fts_ts_data *ts_data = dev_get_drvdata(dev);
+
+    if (!ts_data)
+        return -ENODEV;
+
+    return scnprintf(buf, PAGE_SIZE, "%d\n", ts_data->single_tap_pressed ? 1 : 0);
+}
+
 /* sysfs gesture node
  *   read example: cat  fts_gesture_mode       ---read gesture mode
  *   write example:echo 1 > fts_gesture_mode   --- write gesture mode to 1
@@ -239,10 +289,18 @@ static DEVICE_ATTR(fts_gesture_buf, S_IRUGO | S_IWUSR,
 static DEVICE_ATTR(fts_gesture_bm, S_IRUGO | S_IWUSR,
                    fts_gesture_bm_show, fts_gesture_bm_store);
 
+static DEVICE_ATTR(fts_gesture_single_tap_enabled, S_IRUGO | S_IWUSR,
+                   fts_gesture_single_tap_enabled_show, fts_gesture_single_tap_enabled_store);
+
+static DEVICE_ATTR(fts_gesture_single_tap_pressed, S_IRUGO,
+                   fts_gesture_single_tap_pressed_show, NULL);
+
 static struct attribute *fts_gesture_mode_attrs[] = {
     &dev_attr_fts_gesture_mode.attr,
     &dev_attr_fts_gesture_buf.attr,
     &dev_attr_fts_gesture_bm.attr,
+    &dev_attr_fts_gesture_single_tap_enabled.attr,
+    &dev_attr_fts_gesture_single_tap_pressed.attr,
     NULL,
 };
 
@@ -331,8 +389,11 @@ static void fts_gesture_report(struct input_dev *input_dev, int gesture_id)
         gesture = KEY_GESTURE_U;
         break;
     case GESTURE_SINGLECLICK:
-        gesture = KEY_GESTURE_WAKEUP;
-        break;
+        if (fts_data->single_tap_enabled) {
+            fts_data->single_tap_pressed = 1;
+            sysfs_notify(&fts_data->dev->kobj, NULL, "fts_gesture_single_tap_pressed");
+        }
+        fallthrough;
     case GESTURE_O:
         gesture = KEY_GESTURE_O;
         break;
@@ -537,7 +598,6 @@ int fts_gesture_init(struct fts_ts_data *ts_data)
     input_set_capability(input_dev, EV_KEY, KEY_GESTURE_Z);
     input_set_capability(input_dev, EV_KEY, KEY_GESTURE_C);
     input_set_capability(input_dev, EV_KEY, KEY_GESTURE_FOD);
-    input_set_capability(input_dev, EV_KEY, KEY_GESTURE_WAKEUP);
     input_set_capability(input_dev, EV_KEY, KEY_PALM_TO_SLEEP);
 
 
@@ -556,7 +616,6 @@ int fts_gesture_init(struct fts_ts_data *ts_data)
     __set_bit(KEY_GESTURE_C, input_dev->keybit);
     __set_bit(KEY_GESTURE_Z, input_dev->keybit);
     __set_bit(KEY_GESTURE_FOD, input_dev->keybit);
-    __set_bit(KEY_GESTURE_WAKEUP, input_dev->keybit);
     __set_bit(KEY_PALM_TO_SLEEP, input_dev->keybit);
 
 
